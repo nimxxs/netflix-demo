34622e05dec651382524e122b3aa1cdb22583080
soomin <tnals2199@naver.com>
2024-04-08 17:41:53 +0900

    banner 구현

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,19 @@
+import {useQuery} from '@tanstack/react-query';
+import api from '../utils/api';
+
+// Banner에 들어갈 popular movie
+// 1-1. popular movie api 호출하기
+
+const fetchPopularMovies = () => {
+    return api.get(`/movie/popular`);
+};
+export const usePopularMoviesQuery = () => {
+    return useQuery({
+        queryKey: ['movie-popular'],
+        queryFn: fetchPopularMovies,
+        select:(result) => result.data,
+    });
+};
+
+// 이렇게 hook을 만드는 이유? -> 다른 곳에서 그대로 가져다 쓰기 쉽기 때문.
+// component를 비즈니스 로직과 UI를 분리시키는 게 좋음.
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Banner from './components/Banner/Banner'
 
+// 1. 배너 만들기 => popular 영화를 들고와서 첫 번째 아이템을 보여주자.
+// 1-1. popular movie api 호출하기
+// 2. popular movie 가져오기
+// 3. top rated movie
+// 4. upcoming movie
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div><Banner /></div>
   )
 }
 
