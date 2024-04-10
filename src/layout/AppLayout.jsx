b58d60342146a61ed699eb880efc756454399e91
soomin <tnals2199@naver.com>
2024-04-11 00:37:16 +0900

    키워드 및 페이지네이션 구현

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,39 +1,60 @@
-import React from 'react';
-import {Nav, Navbar, Container, Form, Button} from 'react-bootstrap';
-import {Outlet} from 'react-router-dom';
+import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Nav, Navbar, Container, Form, Button } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  const searchByKeyword = (event) => {
+    event.preventDefault();
+    // url을 바꿔줌 -> keyword가 세팅된 채로 새로고침이 되기 때문에 url자체를 들고오는 게 좋음
+    navigate(`/movies?q=${keyword}`);
+    setKeyword("");
+  };
   return (
     <div>
-    <Navbar expand="lg" className="nav-background">
-      <Container fluid>
-        <Navbar.Brand href="#"><img className="netflix-logo" src="https://static-cdn.jtvnw.net/jtv_user_pictures/bcb0e800-77b3-40d9-9ccb-4195483fb726-profile_image-300x300.png" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll 
-          >
-            <Nav.Link href="/" className='nav-font-color nav-link'>Home</Nav.Link>
-            <Nav.Link href="/Movies" className='nav-font-color nav-link'>Movies</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-          <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
+      <Navbar expand="lg" className="nav-background">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img
+              className="netflix-logo"
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/bcb0e800-77b3-40d9-9ccb-4195483fb726-profile_image-300x300.png"
             />
-            <Button variant="outline-danger">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    {/* router 안에 있는 자손들을 들고 오게 하는 컴포넌트*/}
-    <Outlet />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/" className="nav-font-color nav-link">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/Movies" className="nav-font-color nav-link">
+                Movies
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex" onSubmit={searchByKeyword}>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                value={keyword}
+                onChange={(event) => setKeyword(event.target.value)}
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* router 안에 있는 자손들을 들고 오게 하는 컴포넌트*/}
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
--- src/pages/Moives/MoviePage.jsx
+++ src/pages/Moives/MoviePage.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { React, useState } from "react";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Container, Row, Col, Alert } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
 
+// 경로는 2가지
+// nav바에서 클릭해서 온 경우 -> popularMovie 보여주기
+// keyword를 입력해서 온 경우 -> keyword와 관련된 영화들을 보여줌
+
+// 페이지네이션 설치
+// page state 만들기
+// 페이지네이션 클릭할 때마다 page 바꿔주기
+// page 값이 바뀔 때마다 useSearchMovie에 page까지 넣어서 fetch
 const MoviePage = () => {
-  return (
-    <div>MoviePage</div>
-  )
-}
+  const [query, setQuery] = useSearchParams();
+  const [page, setPage] = useState(1);
+  const keyword = query.get("q");
 
-export default MoviePage
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+
+  const handlePageClick = ({ selected }) => {
+    setPage(selected + 1);
+  };
+
+  console.log("ddd", data);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col lg={4} xs={12}>
+          피터
+        </Col>
+        <Col lg={8} xs={12}>
+          <Row>
+            {data?.results.map((movie, index) => (
+              <Col key={index} lg={4} xs={12}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={data?.total_pages}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+        forcePage={page - 1}
+      />
+    </Container>
+  );
+};
+
+export default MoviePage;
